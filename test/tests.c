9f42d71945989289f0b7de0edc53febff1b47934
NoFunny <chunikhin_1999@mail.ru>
2018-04-22 20:31:07 +0700

    Add test for rook

--- test/tests.c
+++ test/tests.c
@@ -84,3 +84,79 @@ CTEST(move_suite, move_pawn) // Ход пешки
 	ASSERT_EQUAL(expected3, check3);
 	ASSERT_EQUAL(expected4, check4);
 }
+
+CTEST(move_suite, move_rook) // Ход Ладьи
+{
+	//GIVEN
+	char board[8][8] = {
+		"rhbqkbhr",
+		"........",
+		".....P..",
+		"........",
+		"........",
+		"........",
+		"PPPPP.PP",
+		"RHBQKBHR" 
+	};
+	
+	
+	//WHEN//
+	int coordinates[4];
+
+	//Ход вперёд 
+	coordinates[0] = 7;
+	coordinates[1] = 0;     
+	coordinates[2] = 7;
+	coordinates[3] = 3;
+	
+	int check1 = move_checking(board, coordinates); 
+	moving(board, coordinates); 	
+
+	//Ход назад
+	coordinates[0] = 7;
+	coordinates[1] = 3;    
+	coordinates[2] = 7;
+	coordinates[3] = 2;
+
+	int check2 = move_checking(board, coordinates); 
+	moving(board, coordinates); 	
+
+	//Ход влево
+	coordinates[0] = 7;
+	coordinates[1] = 2;     
+	coordinates[2] = 6;
+	coordinates[3] = 2;
+
+	int check3 = move_checking(board, coordinates); 
+	moving(board, coordinates); 	
+
+	//Атака
+	coordinates[0] = 6;
+	coordinates[1] = 2;     
+	coordinates[2] = 5;
+	coordinates[3] = 2;
+
+	int check4 = move_checking(board, coordinates);
+	moving(board, coordinates);
+
+	//Ход по диагонале 
+	coordinates[0] = 5;
+	coordinates[1] = 2;     
+	coordinates[2] = 4;
+	coordinates[3] = 3;
+
+	int check5 = move_checking(board, coordinates); 
+
+	//THEN//
+	const int expected1 = 0;
+	const int expected2 = 0;
+	const int expected3 = 0;
+	const int expected4 = 0;
+	const int expected5 = 1;
+
+	ASSERT_EQUAL(expected1, check1);
+	ASSERT_EQUAL(expected2, check2);
+	ASSERT_EQUAL(expected3, check3);
+	ASSERT_EQUAL(expected4, check4);
+	ASSERT_EQUAL(expected5, check5);
+}
