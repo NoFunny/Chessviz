1d003c16deeb5d36d9ac0e2d92359c02eb0e5135
NoFunny <chunikhin_1999@mail.ru>
2018-04-22 20:52:21 +0700

    Modified make and add test for horse

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: bin/prog bin/test
 
 bin/prog: build/main.o build/check.o build/command.o 
-	gcc -Wall -Werror build/main.o build/check.o build/command.o -o prog
+	gcc -Wall -Werror build/main.o build/check.o build/command.o -o bin/prog
 
 build/main.o: src/main.c
 	gcc -Wall -Werror -c src/main.c -o build/main.o 
@@ -12,8 +12,11 @@ build/check.o: src/check.c
 build/command.o: src/command.c
 	gcc -Wall -Werror -c src/command.c -o build/command.o 
 	
-bin/test: build/command.o build/check.o build/first_test.o 
-	gcc -Wall build/command.o build/check.o build/first_test.o -o bin/test
+bin/test: build/command.o build/check.o  build/tests.o build/first_test.o 
+	gcc -Wall build/tests.o build/command.o build/check.o build/first_test.o -o bin/test
+
+build/test.o: test/tests.c
+	gcc -Wall -c test/tests.c -o build/tests.o -Ithirdparty -Isrc
 
 build/first_test.o: test/first_test.c
 	gcc -Wall -c test/first_test.c -o build/first_test.o -Ithirdparty
--- test/tests.c
+++ test/tests.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <ctest.h>
-#include "commands.h"
+#include "command.h"
 #include "check.h"
 
 CTEST(move_suite, move_pawn) // Ход пешки 
@@ -152,6 +152,81 @@ CTEST(move_suite, move_rook) // Ход Ладьи
 	const int expected2 = 0;
 	const int expected3 = 0;
 	const int expected4 = 0;
+	const int expected5 = 1;
+
+	ASSERT_EQUAL(expected1, check1);
+	ASSERT_EQUAL(expected2, check2);
+	ASSERT_EQUAL(expected3, check3);
+	ASSERT_EQUAL(expected4, check4);
+	ASSERT_EQUAL(expected5, check5);
+}
+
+CTEST(move_suite, move_hourse) // Ход коня
+{
+	//GIVEN
+	char board[8][8] = {
+		"rhbqkbhr",
+		"pppppppp",
+		"........",
+		"........",
+		"....P...",
+		"........",
+		"PPPP.PPP",
+		"RHBQKBHR" 
+	};
+	
+	//WHEN//
+
+	int coordinates[4];
+
+	//ход Г(перевернутой)
+	coordinates[0] = 6;
+	coordinates[1] = 0;     
+	coordinates[2] = 5;
+	coordinates[3] = 2;
+
+	int check1 = move_checking(board, coordinates); 
+	moving(board, coordinates);
+
+	//ход Г
+	coordinates[0] = 1;
+	coordinates[1] = 0;    
+	coordinates[2] = 2;
+	coordinates[3] = 2;
+
+	int check2 = move_checking(board, coordinates); 
+	moving(board, coordinates);
+
+	//Атака
+	coordinates[0] = 5;
+	coordinates[1] = 2;    
+	coordinates[2] = 4;
+	coordinates[3] = 4;
+
+	int check3 = move_checking(board, coordinates); 
+	moving(board, coordinates);
+
+	//ложный ход #1
+	coordinates[0] = 4;
+	coordinates[1] = 4;    
+	coordinates[2] = 4;
+	coordinates[3] = 5;
+
+	int check4 = move_checking(board, coordinates); 
+
+	//ложный ход #2
+	coordinates[0] = 4;
+	coordinates[1] = 4;     
+	coordinates[2] = 4;
+	coordinates[3] = 3;
+
+	int check5 = move_checking(board, coordinates); 
+	
+	//THEN
+	const int expected1 = 0;
+	const int expected2 = 0;
+	const int expected3 = 0;
+	const int expected4 = 1;
 	const int expected5 = 1;
 
 	ASSERT_EQUAL(expected1, check1);
